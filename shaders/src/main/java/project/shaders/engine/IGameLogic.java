e4bc808415127f3269de823c3584e2643685bc81
hash <hash@hash-desktop>
2017-04-26 23:27:27 +0300

    Window and Game Loop

---
+++ shaders/src/main/java/project/shaders/engine/IGameLogic.java
@@ -0,0 +1,12 @@
+package project.shaders.engine;
+
+public interface IGameLogic {
+
+    void init() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+    
+    void render(Window window);
+}
---
+++ shaders/src/main/java/project/shaders/game/Main.java
@@ -0,0 +1,22 @@
+package project.shaders.game;
+
+import project.shaders.engine.GameEngine;
+import project.shaders.engine.IGameLogic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			boolean vSync = true;
+			IGameLogic gameLogic = new DummyGame();
+			GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+			gameEng.start();
+		} catch (Exception excp) {
+			excp.printStackTrace();
+			System.exit(-1);
+		}
+
+	}
+
+}
