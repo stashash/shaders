4ba673e5da4efaef49d0866d8fd918961bed3684
stas_hash <stas_hash@DESKTOP-SBJGDM5.Dlink>
2017-10-18 23:13:00 +0300

    add shader vreate

--- shaders/src/main/java/project/shader/graph/ShaderProgram.java
+++ shaders/src/main/java/project/shader/graph/ShaderProgram.java
@@ -11,5 +11,53 @@ public class ShaderProgram {
 		if (programId == 0)
 				throw new Exception("Could not create shader");
 	}
+	
+	public void createVertexShader(String shaderCode) throws Exception{
+		vertexShaderId = createShader(shaderCode,GL_VERTEX_SHADER);
+	}
+	
+	
+	public void createFragmentShader(String shaderCode) throws Exception{
+		vertexShaderId = createShader(shaderCode,GL_FRAGMENT_SHADER);
+	}
+
+	private int createShader(String shaderCode, int shaderType)throws Exception {
+		int shaderId = glCreateShader(shaderType);
+		if (shaderId == 0)
+			throw new Exception("Error creating shader. Type - " + shaderType);
+		
+		glShaderSource(shaderId, shaderCode);
+		glCompileShader(shaderId);
+		
+		if (glGetShaderi(shaderId, GL_COMPILE_STATUS) == 0)
+			throw new Exception("Error compile shader code" + glGetShaderInfoLog(shaderId, 1024));
+		
+		glAttachShader(programId, shaderId);
+		return shaderId;
+	}
+	
+	public void link() throws Exception{
+		glLinkProgram(programId);
+		
+		if (glGetProgrami(programId, GL_LINK_STATUS) == 0)
+			throw new Exception("Error Linked shader code " + glGetProgramInfoLog(programId, 1024));
+		
+		if (vertexShaderId != 0)
+			glDetachShader(programId, vertexShaderId);
+		if (fragmentShaderId != 0)
+			glDetachShader(programId, fragmentShaderId);
+		
+	}
+	public void bind() {
+		glUseProgram(programId);
+	}
+	public void unbind() {
+		glUseProgram(0);
+	}
+	public void cleanup() {
+		unbind();
+		if (programId !=0)
+			glDeleteProgram(programId);
+	}
 
 }
